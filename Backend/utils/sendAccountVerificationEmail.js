3a28a424965804c52d6c81ccfd5e6229546739fb
Sagar <sp120727@gmail.com>
2026-04-15 15:55:43 +0530

    account_verification

--- Backend/utils/sendAccountVerificationEmail.js
+++ Backend/utils/sendAccountVerificationEmail.js
@@ -22,7 +22,7 @@ const sendAccountVerificationEmail = async (to, verificationToken) => {
       subject: "Account Verification Token",
       html: `<p> You are receiving this email because you (or someone else) have requested to verify your account.</p>
             <p>Please click on the following link, or paste this into your browser to complete the process:</p>
-            <p>https://bloggy-tech-app-backend.vercel.app/reset-password/${verificationToken}</p>
+            <p>https://bloggy-tech-app-backend.vercel.app/verify-account/${verificationToken}</p>
             <p>If you did not request this, please ignore this email and your password will remain unchanged.</p> `,
     };
 
